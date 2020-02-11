dbc45c0a087ab4dd5d90b35b013db0aadb56eccf
utit <triyaid@gmail.com>
2020-02-11 20:38:46 +0700

    perbaikan pada pembuatan file .conf

--- albanivirtualhostgenerator.sh
+++ albanivirtualhostgenerator.sh
@@ -1,23 +1,29 @@
 #!bash
 
-##mulai
+##Membuat nama domain
 echo Masukan nama domain yang akan dibuat contoh: albani.id !
 read namaDomain
-##membuat direktori root proyek
+if [ -z "$namaDomain" ]; then
+	echo STOP...  Proses dihentikan karena kamu nggak nentuin nama domainnya :'('
+	exit
+fi
+
 sudo mkdir  /var/www/html/$namaDomain
 sudo chown -R $USER:$USER /var/www/html/$namaDomain
 sudo chmod -R 755 /var/www/html/$namaDomain
 echo '<center style="margin-top:50px">Alhamdulillah, kamu berhasil membuat virtual host/domain <b>'.$namaDomain.'</b></center>' >> /var/www/html/$namaDomain/index.html
 
-sudo tee -a /etc/apache2/sites-available/$namaDomain.conf << 'EOF'
+#eksekusi
+
+sudo tee -a /etc/apache2/sites-available/$namaDomain.conf << EOF
 <VirtualHost *:80>
 	
 	ServerAdmin admin@localhost
-	ServerName $namaDomain.conf
-	ServerAlias www.$namaDomain.conf
-	DocumentRoot /var/www/html/$namaDomain.conf
+	ServerName $namaDomain
+	ServerAlias www.$namaDomain
+	DocumentRoot /var/www/html/$namaDomain
 	
-	<Directory "/var/www/html/$namaDomain.conf">
+	<Directory "/var/www/html/$namaDomain">
 		Allowoverride All
 	</Directory>
 
@@ -31,4 +37,8 @@ sudo a2ensite $namaDomain.conf
 
 sudo systemctl restart apache2
 
-
+printf '\n \n \n'
+printf '============================ Albani Project ============================ \n'
+echo Alhamdulillah, VHost berhasil dibuat
+echo Dengan direktori kerja di /var/www/html/$namaDomain
+echo Jangan lupa untuk menambahkan domain $namaDomain beserta ip pada file /etc/hosts
